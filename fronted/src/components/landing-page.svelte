1af77365891f488b9116d3db7076fe92e969fa3f
jp1593 <juanpablo08082002@hotmail.com>
2024-12-03 17:51:46 -0600

    landing page and preview add

---
+++ fronted/src/components/landing-page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<div class="landing-page">
+  <h1>Welcome to the Landing Page!</h1>
+  <p>This is the page that appears after the home screen disappears.</p>
+</div>
+
+<style>
+  .landing-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: lightcoral;
+    color: white;
+    text-align: center;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+  }
+</style>
---
+++ fronted/src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+  import LandingPage from "../components/landing-page.svelte";
+
+  let showHomeScreen = true;
+  let showLandingPage = false;
+
+  onMount(() => {
+    const timer = setTimeout(() => {
+      showHomeScreen = false;
+      showLandingPage = true;
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  });
+</script>
+
+{#if showHomeScreen}
+  <div class="preview" transition:fade></div>
+{:else if showLandingPage}
+  <div class="home">
+    <LandingPage />
+  </div>
+{/if}
+
+<style>
+  .preview {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-image: url("../assets/ShopFlow.jpeg");
+    background-size: cover;
+    background-position: center;
+  }
+
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+</style>
